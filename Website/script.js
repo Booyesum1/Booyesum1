3f8988af0f37feea8e1c068030701835cac7bb6d
Booyesum1 <97325299+Booyesum1@users.noreply.github.com>
2022-11-29 21:12:26 -0500

    Did more work on the games and more improvements to the Resume page.

---
+++ Website/script.js
@@ -0,0 +1,33 @@
+//This is the class declaration
+class User {
+    //This is the constructor statement for the class User
+    constructor (
+        //It defines the perameters of the class User
+    firstName,
+    lastName,
+    email,
+    birthDate,
+    streetAddress,
+    city,
+    state,
+    zip,
+    userName,
+    password,
+    character,
+) {
+    //Defines Properties
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.email = email,
+    this.birthDate = birthDate,
+    this.streetAddress = streetAddress,
+    this.city = city,
+    this.state = state,
+    this.zip = zip,
+    this.userName = userName,
+    this.password = password,
+    this.character = character
+
+};
+
+}
