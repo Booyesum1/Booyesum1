f926044b90aa825498e2ff4224f0ef180acb70e4
Booyesum1 <97325299+Booyesum1@users.noreply.github.com>
2022-12-02 17:59:48 -0500

    adding the card select functions and the images.

--- memoryGame/memoryGame.js
+++ memoryGame/memoryGame.js
@@ -1,51 +1,51 @@
 const cardArray = [
     {
-        name: 'fries',
-        img: 'image',
+        name: 'csmpreston',
+        img: 'images/csmapreston.jpeg',
     },
     {
-        name: 'cheeseburger',
-        img: 'image',
+        name: 'franken',
+        img: 'images/franken.jpeg',
     },
     {
-        name: 'pizza',
-        img: 'image',
+        name: 'jacket',
+        img: 'images/jacket.jpeg',
     },
     {
-        name: 'milk shake',
-        img: 'image',
+        name: 'redcap',
+        img: 'images/redcap.jpeg',
     },
     {
-        name: 'ice cream',
-        img: 'image',
+        name: 'scarf',
+        img: 'images/scarf.jpeg',
     },
     {
-        name: 'hot dog',
-        img: 'image',
+        name: 'tall',
+        img: 'images/tall.jpeg',
     },
     {
-        name: 'fries',
-        img: 'image',
+        name: 'csmpreston',
+        img: 'images/csmapreston.jpeg',
     },
     {
-        name: 'cheeseburger',
-        img: 'image',
+        name: 'franken',
+        img: 'images/franken.jpeg',
     },
     {
-        name: 'pizza',
-        img: 'image',
+        name: 'jacket',
+        img: 'images/jacket.jpeg',
     },
     {
-        name: 'milk shake',
-        img: 'image',
+        name: 'redcap',
+        img: 'images/redcap.jpeg',
     },
     {
-        name: 'ice cream',
-        img: 'image',
+        name: 'scarf',
+        img: 'images/scarf.jpeg',
     },
     {
-        name: 'hot dog',
-        img: 'image',
+        name: 'tall',
+        img: 'images/tall.jpeg',
     }
 
 ]
@@ -53,13 +53,36 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+const cardsChosen = []
+const cardsChosenIds = []
 
 function createBoard() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
-        card.setAttribute('src', 'image')
+        card.setAttribute('src', 'images/back.jpeg')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
     }
 }
 createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    document.querySelectorAll('img')
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match!')
+        // cards[cardsChosenIds[[0]]].setAttribute('src', images/)
+    }
+}
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardArray[cardId].name
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout( checkMatch, 500)
+    }
+}
