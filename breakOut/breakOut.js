3eeef093a87125e27f3f7cd88e0e502600f680d5
Booyesum1 <97325299+Booyesum1@users.noreply.github.com>
2022-12-03 21:08:50 -0500

    Finished the break out game. Will come back and make improvements.

--- breakOut/breakOut.js
+++ breakOut/breakOut.js
@@ -1,6 +1,20 @@
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100
 const blockHeight = 20
+const ballDiameter = 20
+const boardWidth = 560
+const boardHeight = 300
+let timerId
+let xDirection = 2
+let yDirection = 2
+let score = 0
+
+const userStart = [230, 10]
+let userCurrentPosition = userStart
+
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
 
 //create block
 class Block {
@@ -45,3 +59,122 @@ function addBlocks() {
 addBlocks()
 
 //add user
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
+
+//draw the user
+function drawUser() {
+    user.style.left = userCurrentPosition[0] + 'px'
+    user.style.bottom = userCurrentPosition[1] + 'px'   
+}
+
+//draw the ball
+function drawBall() {
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+
+//move user
+function moveUser(e) {
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (userCurrentPosition[0] > 0) {
+                userCurrentPosition[0] -= 10
+                drawUser()
+            }
+            break;
+        case 'ArrowRight':
+            if (userCurrentPosition[0] < boardWidth - blockWidth) {
+                userCurrentPosition[0] += 10
+                drawUser()
+            }
+            break;
+    }
+}
+
+document.addEventListener('keydown', moveUser)
+
+// add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+//move ball
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+    checkForCollisions()
+}
+
+timerId = setInterval(moveBall, 30)
+
+//check for collisions
+function checkForCollisions() {
+    //check for block collisions
+    for (let i = 0; i < blocks.length; i++) {
+        if (
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0] &&
+            (ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft && ballCurrentPosition[1] < blocks[i].topLeft[1])
+        ) {
+            const allBlocks = Array.from(document.querySelectorAll('.block'))
+            allBlocks[i].classList.remove('block')
+            blocks.splice(i, 1)
+            changeDirection()
+            score++
+            scoreDisplay.innerHTML = score
+
+            //check for win
+            if (blocks.length === 0) {
+                scoreDisplay.innerHTML = 'YOU WIN!!'
+                clearInterval(timerId)
+                document. removeEventListener('keydown', moveUser)
+            }
+        }
+    }
+    //check for wall collisions
+    if (
+        ballCurrentPosition[0] >= (boardWidth - ballDiameter) ||
+        ballCurrentPosition[1] >= (boardHeight - ballDiameter) ||
+        ballCurrentPosition[0] <= 0
+        ) {
+            changeDirection()
+        }
+    
+    //check for user Collisions
+    if (
+        (ballCurrentPosition[0] > userCurrentPosition[0] && ballCurrentPosition[0] < userCurrentPosition[0] + blockWidth) &&
+        (ballCurrentPosition[1] > userCurrentPosition[1] && ballCurrentPosition[1] < userCurrentPosition[1] + blockHeight)
+    ) {
+        changeDirection()
+    }
+
+    //check for game over
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = 'You Lose'
+        document.removeEventListener('keydown', moveUser)
+    }
+}
+
+function changeDirection() {
+    if (xDirection === 2 && yDirection === 2) {
+        yDirection = -2
+        return
+    }
+    if (xDirection === 2 && yDirection === -2) {
+        xDirection = -2
+        return
+    }
+    if (xDirection === -2 && yDirection === -2) {
+        yDirection = 2
+        return
+    }
+    if (xDirection === -2 && yDirection === 2) {
+        xDirection = 2
+        return
+    }
+}
